8259cd49efff2266abaabbe205249ab7532a93ca
AnkushCodes <ankushkanchar07@gmail.com>
2018-02-21 16:45:36 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick=function(){
         
 };
         
- request.open('GET',"http://ankushkanchar07.imad.hasura-app.io/counter",true);
+ request.open('GET','http://ankushkanchar07.imad.hasura-app.io/counter',true);
  request.send(null);
 
 };
