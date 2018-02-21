4108d6af73d0e8a271e7a94798dbf3f5829f7fb6
AnkushCodes <ankushkanchar07@gmail.com>
2018-02-21 16:42:53 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var button =document.getElementById("counter");
+var button =document.getElementById('counter');
 
 
 
@@ -10,10 +10,10 @@ button.onclick=function(){
     var request=new XMLHttpRequest();
     
     request.onreadystatechange = function(){
-        if(request.readyState===XMLHttepRequest.DONE){
+        if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200){
                 var counter = request.responseText;
-                var span=document.getElementById("count");
+                var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
 };
             }
