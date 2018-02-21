53384e9392c76f1e0ca32fcfaca831b2ecfa926a
AnkushCodes <ankushkanchar07@gmail.com>
2018-02-21 14:26:53 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML='New Value';
+
+var img=document.getElementById('img');
+
+img.onClick=function(){
+    img.marginLeft='100px'
+};
