60f7df9989f86d17dd94bba217040ecee8edc34a
AnkushCodes <ankushkanchar07@gmail.com>
2018-02-25 11:35:57 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,39 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool =requir('pg').Pool;
-
-var config={
-    user:'ankushkanchar07',
-    database:'ankushkanchar07',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
-}
 
 var app = express();
-
 app.use(morgan('combined'));
-
-
-
-
-
-
-
-
-var pool =new Pool(config);
-
-app.get('/test-db',function(req,res){
-   pool.query('SELECT * FROM test',function(err,result){
-    if(err){
-        res.status(500).send(err.toString());
-    }  else{
-        res.send(JSON.stringify(result,rows));
-    } 
-   }); 
-});
-
 
 var articles={ 
       'article-one':{
@@ -82,7 +52,7 @@ var articles={
     };
    
 
-function createTemplate(data){
+  function createTemplate(data){
 
       var title=data.title;
       var date=data.date;
@@ -125,19 +95,9 @@ function createTemplate(data){
   }
 
 
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-var counter=0;
-
-app.get('/counter',function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -145,20 +105,6 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-
-var names=[];
-
-app.get('/submit-name',function(req,res){
-   var name=req.query.name;
-   names.push(name);
-   res.send(JSON.stringify(names));
-    
 });
 
 app.get('/:articleName',function(req,res){
