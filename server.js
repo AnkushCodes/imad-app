7ada1a958ac7ce50e9003c60c3b29656870d93ad
AnkushCodes <ankushkanchar07@gmail.com>
2018-02-25 11:22:37 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
+
+
+
+
+var Pool =requir('path').Pool;
+
+var config={
+    user:'ankushkanchar07',
+    database:'ankushkanchar07',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+}
+
+var pool =new Pool(config);
+
+app.get('/test-db',function(req,res){
+   pool.query('SELECT * FROM test',function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    }  else{
+        res.send(JSON.stringify(result,rows));
+    } 
+   }); 
+});
 
 
 var articles={ 
