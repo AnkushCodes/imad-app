904d92b3f6f8bfb4ae4f81d1f6fc7eeeb3c53d01
AnkushCodes <ankushkanchar07@gmail.com>
2018-02-27 21:09:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ pool.query('SELECT * FROM article', function(err,result){
 
 
 
-function hash(input,salt){
+function hash  (input,salt){
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
