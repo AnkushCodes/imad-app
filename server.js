458f4a7b995dfde7747ad6c2b3c6e2b1e51bff60
AnkushCodes <ankushkanchar07@gmail.com>
2018-02-28 23:10:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.post('/login',function(req,res){
                 var dbString=result.rows[0].password;
                 var salt=dbString.split('$')[2];
                 var hashedPassword=hash(password,salt);
-                if(hashedPassowrd===dbString){
+                if(hashedPassoword===dbString){
                     res.send('user created sucessfully')
                 }else{
                       res.send(403).send('username / password invalid');
