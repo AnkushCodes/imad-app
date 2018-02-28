587ce8a761a6c727b38a89e39a29483d82f6b6bf
AnkushCodes <ankushkanchar07@gmail.com>
2018-03-01 01:32:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.get('/check-login', function(req,res){
     if(req.session && req.session.auth && req.session.auth.userId){
         res.send('you are logged in'+req.session.auth.userId.toString());
     }else{
-        req.send('you are not logged in');
+        res.send('you are not logged in');
     }
     
 });
