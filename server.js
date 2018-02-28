d0ac325e31e51ff492e6295cde5899f23c12f403
AnkushCodes <ankushkanchar07@gmail.com>
2018-03-01 01:37:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.get('/check-login', function(req,res){
    
  app.get('/logout',function(req,res){
    delete req.session.auth;
-   res.send('loged out');
+   res.send('Logged out');
  });
  
  
