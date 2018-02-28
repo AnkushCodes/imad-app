0715ca67c0804d8763377f4db0b3e0958207f03e
AnkushCodes <ankushkanchar07@gmail.com>
2018-03-01 01:17:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@
                    if(request.readyState===XMLHttpRequest.DONE){
                         if(request.status===200){
                            
-                           req.session.auth = { userId: result.rows[0].id};
+                           
                            
                            alert('loged in sucessfully');
                         }else if(request.status===403){
